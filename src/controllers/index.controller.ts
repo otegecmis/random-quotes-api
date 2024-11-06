851fa55e88d91a232ac8ae53992c187e7ae87674
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-11-06 15:42:26 +0300

    chore: create `service` and `controller` for `index`

---
+++ src/controllers/index.controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+
+import indexService from "../services/index.service";
+
+class IndexController {
+    async welcomePage(req: Request, res: Response): Promise<void> {
+        try {
+            const result: string = await indexService.welcomePage();
+
+            res.json({
+                message: result
+            })
+        } catch (error) {
+            res.status(500).json("An error occurred while fetching the welcome message.");
+        }
+    }
+}
+
+export default new IndexController();
