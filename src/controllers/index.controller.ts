74abb57e83abfb7173d9c0c39e54b8e54424fc4d
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-11-06 15:44:44 +0300

    chore: update `JSON` output

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -1,19 +1,23 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 
-import indexService from "../services/index.service";
+import indexService from '../services/index.service';
 
 class IndexController {
-    async welcomePage(req: Request, res: Response): Promise<void> {
-        try {
-            const result: string = await indexService.welcomePage();
+  async welcomePage(req: Request, res: Response): Promise<void> {
+    try {
+      const result: string = await indexService.welcomePage();
 
-            res.json({
-                message: result
-            })
-        } catch (error) {
-            res.status(500).json("An error occurred while fetching the welcome message.");
-        }
+      res.json({
+        result: {
+          message: result,
+        },
+      });
+    } catch (error) {
+      res
+        .status(500)
+        .json('An error occurred while fetching the welcome message.');
     }
+  }
 }
 
 export default new IndexController();
