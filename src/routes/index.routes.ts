c7032698cd18dff1ba97a6950746e400aed235f0
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-11-07 01:05:47 +0300

    chore: add `Swagger` routes to `index` routes

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,9 +1,11 @@
 import express, { Router } from 'express';
 
 import authRoutes from './auth.routes';
+import swaggerRoutes from './swagger.routes';
 
 const router: Router = express.Router();
 
+router.use('/swagger', swaggerRoutes);
 router.use('/auth', authRoutes);
 
 export default router;
