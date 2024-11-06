0ce6c5c5c5a1d86ca19ca0a39a9282056d1517c6
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-11-06 19:43:01 +0300

    chore: add `try-catch` `error handling` to `user repository`

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,12 +1,26 @@
+import createError from 'http-errors';
+
 import User, { IUser } from '../models/User';
 
 class UserRepository {
   async createUser(user: IUser): Promise<IUser> {
-    return await User.create(user);
+    try {
+      return await User.create(user);
+    } catch (error: any) {
+      if (error.code === 11000 && error.keyPattern?.email) {
+        throw createError(400, 'Please use a different email.');
+      }
+
+      throw createError(500, 'Unable to create the user at the moment.');
+    }
   }
 
   async getUserByEmail(email: string): Promise<IUser | null> {
-    return await User.findOne({ email });
+    try {
+      return await User.findOne({ email });
+    } catch (error) {
+      throw createError(500, 'Unable to fetch the user at the moment.');
+    }
   }
 }
 
