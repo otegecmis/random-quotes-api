91bf60434a73a63b611f97968a70e1bddcee4d21
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-11-06 19:52:59 +0300

    chore: add `auth` routes to `index` routes

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,5 +1,9 @@
 import express, { Router } from 'express';
 
+import authRoutes from './auth.routes';
+
 const router: Router = express.Router();
+
+router.use('/auth', authRoutes);
 
 export default router;
