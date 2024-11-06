75de1ff0f6eda100936d106bf28b307c6feea82a
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-11-06 19:40:05 +0300

    chore: remove `index` controller from `index routes`

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,8 +1,5 @@
 import express, { Router } from 'express';
-import indexController from '../controllers/index.controller';
 
 const router: Router = express.Router();
-
-router.get('/', indexController.welcomePage);
 
 export default router;
