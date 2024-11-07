b36b85a0fadc3460b6d17764874107d4e73edec2
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-11-07 19:23:03 +0300

    chore: add validation to auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,10 +1,27 @@
 import { NextFunction, Request, Response } from 'express';
 
 import authService from '../services/auth.service';
+import {
+  refreshTokenValidationSchema,
+  signInValidationSchema,
+  signUpValidationSchema,
+} from '../validations/auth.validation';
 
 class AuthController {
   async signUp(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
+      const { error } = signUpValidationSchema.validate(req.body);
+
+      if (error) {
+        res.status(400).json({
+          result: {
+            message: error.details[0].message,
+          },
+        });
+
+        return;
+      }
+
       const { email, password } = req.body;
       const result = await authService.signUp(email, password);
 
@@ -18,6 +35,18 @@ class AuthController {
 
   async signIn(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
+      const { error } = signInValidationSchema.validate(req.body);
+
+      if (error) {
+        res.status(400).json({
+          result: {
+            message: error.details[0].message,
+          },
+        });
+
+        return;
+      }
+
       const { email, password } = req.body;
       const result = await authService.signIn(email, password);
 
@@ -35,6 +64,18 @@ class AuthController {
     next: NextFunction,
   ): Promise<void> {
     try {
+      const { error } = refreshTokenValidationSchema.validate(req.body);
+
+      if (error) {
+        res.status(400).json({
+          result: {
+            message: error.details[0].message,
+          },
+        });
+
+        return;
+      }
+
       const { refreshToken } = req.body;
       const result = await authService.refresh(refreshToken);
 
