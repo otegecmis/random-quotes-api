e307ad29b5b8d73c828c7513bc14df00c2e1a6b6
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-11-07 16:58:21 +0300

    refactor: move `Swagger` routes to `index` routes

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,11 +1,16 @@
 import express, { Router } from 'express';
 
+import config from '../config/index.config';
+import { serve, setup } from '../config/swagger.config';
+
 import authRoutes from './auth.routes';
-import swaggerRoutes from './swagger.routes';
 
 const router: Router = express.Router();
 
-router.use('/swagger', swaggerRoutes);
+if (config.server.node_env === 'development') {
+  router.use('/', serve, setup);
+}
+
 router.use('/auth', authRoutes);
 
 export default router;
