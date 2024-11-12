0504e39fbbeb7ac9c55ecb63687d0728f9fe3cf7
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-11-12 23:28:28 +0300

    chore: create `quotes` routes and add to `index` routes

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -2,10 +2,12 @@ import express, { Router } from 'express';
 
 import swaggerRoutes from './swagger.routes';
 import authRoutes from './auth.routes';
+import quotesRoutes from './quotes.routes';
 
 const router: Router = express.Router();
 
 router.use('/swagger', swaggerRoutes);
 router.use('/auth', authRoutes);
+router.use('/quotes', quotesRoutes);
 
 export default router;
