2f4875466ed136b045077778e101fe0e6382581e
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-11-13 00:53:52 +0300

    chore: add missing `routes` for `quotes`

--- src/routes/quotes.routes.ts
+++ src/routes/quotes.routes.ts
@@ -32,6 +32,28 @@ router.post('/', quotesController.createQuotes);
 
 /**
  * @swagger
+ * /api/quotes:
+ *   get:
+ *     summary: Get Quotes
+ *     description:
+ *     tags: [Quotes]
+ *     parameters:
+ *       - in: query
+ *         name: currentPage
+ *         schema:
+ *           type: int
+ *       - in: query
+ *         name: perPage
+ *         schema:
+ *           type: int
+ *     responses:
+ *       200:
+ *         description: Ok
+ */
+router.get('/', quotesController.getQuotes);
+
+/**
+ * @swagger
  * /api/quotes/random:
  *   get:
  *     summary: Random Quote
@@ -42,6 +64,58 @@ router.post('/', quotesController.createQuotes);
  *         description: OK
  */
 router.get('/random', quotesController.randomQuotes);
+
+/**
+ * @swagger
+ * /api/quotes/{id}:
+ *   get:
+ *     summary: Get Quote
+ *     description:
+ *     tags: [Quotes]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Ok
+ */
+router.get('/:id', quotesController.getQuote);
+
+/**
+ * @swagger
+ * /api/quotes/{id}:
+ *   put:
+ *     summary: Update Quote
+ *     description:
+ *     tags: [Quotes]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               quote:
+ *                 type: string
+ *               author:
+ *                 type: string
+ *             example:
+ *               quote: "Tıpkı kullanılmayan demirin paslanması ve suyun soğukta donması ya da kokuşması gibi kullanılmayan zihin de bozulur."
+ *               author: "Leonardo di ser Piero da Vinci"
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Ok
+ */
+router.put('/:id', quotesController.updateQuote);
 
 /**
  * @swagger
