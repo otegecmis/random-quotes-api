ccdbc1e8faab1c0b30233af671ffd25b49b266fe
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-11-13 01:08:50 +0300

    chore: create `auth check middleware` and implement in necessary `routes`

---
+++ src/middleware/auth-check.middleware.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from 'express';
+import createError from 'http-errors';
+
+import tokenService from '../services/token.service';
+
+interface AuthRequest extends Request {
+  payload?: any;
+}
+
+const authCheck = {
+  isSignIn: async (
+    req: AuthRequest,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
+    try {
+      const authHeader = req.headers['authorization'];
+
+      if (!authHeader) throw createError.Unauthorized('Please log in.');
+
+      const bearerToken = authHeader.split(' ');
+      req.payload = await tokenService.verifyAccessToken(bearerToken[1]);
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  },
+};
+
+export default authCheck;
