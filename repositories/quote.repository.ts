7ade25c2f45765d354238ba0b723247ca0193214
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-11-16 20:35:10 +0300

    chore: update `quotes` service and repository for `endpoint responses` (#36)

--- repositories/quote.repository.ts
+++ repositories/quote.repository.ts
@@ -1,4 +1,3 @@
-import mongoose from 'mongoose';
 import createError from 'http-errors';
 
 import Quote, { IQuote } from '../models/Quote';
@@ -12,11 +11,22 @@ class QuoteRepository {
     }
   }
 
+  async getQuoteByID(id: string): Promise<IQuote | null> {
+    try {
+      return await Quote.findById(id).exec();
+    } catch (error: any) {
+      throw createError(500, `${error.message}`);
+    }
+  }
+
   async getQuotes(currentPage: number, perPage: number) {
     try {
       const skip = (currentPage - 1) * perPage;
-      const quotes = await Quote.find().skip(skip).limit(perPage).exec();
-      const totalQuotes = await Quote.countDocuments();
+      const quotes = await Quote.find({ status: 'active' })
+        .skip(skip)
+        .limit(perPage)
+        .exec();
+      const totalQuotes = await Quote.countDocuments({ status: 'active' });
 
       return {
         quotes,
@@ -31,46 +41,34 @@ class QuoteRepository {
 
   async getRandomQuote(): Promise<IQuote | null> {
     try {
-      const count = await Quote.countDocuments();
+      const count = await Quote.countDocuments({ status: 'active' });
+
+      if (count === 0) {
+        throw createError.NotFound(
+          'Currently, there are no quotes available to display.',
+        );
+      }
+
       const randomIndex = Math.floor(Math.random() * count);
 
-      return await Quote.findOne().skip(randomIndex);
+      return await Quote.findOne({ status: 'active' }).skip(randomIndex).exec();
     } catch (error: any) {
       throw createError(500, `${error.message}`);
     }
   }
 
-  async getQuote(id: string): Promise<IQuote | null> {
+  async updateQuote(id: string, quote: IQuote): Promise<IQuote> {
     try {
-      if (!mongoose.Types.ObjectId.isValid(id)) {
-        throw createError(400, 'Invalid ID format.');
-      }
-
-      const quote = await Quote.findById(id).exec();
-
-      if (!quote) {
-        throw createError(404, 'Quote not found.');
-      }
-
-      return quote;
-    } catch (error: any) {
-      throw createError(500, `${error.message}`);
-    }
-  }
-
-  async updateQuote(id: string, quote: IQuote): Promise<IQuote | null> {
-    try {
-      if (!mongoose.Types.ObjectId.isValid(id)) {
-        throw createError(400, 'Invalid ID format');
-      }
-
       const updatedQuote = await Quote.findByIdAndUpdate(id, quote, {
         new: true,
         runValidators: true,
       }).exec();
 
       if (!updatedQuote) {
-        throw createError(404, 'Quote not found.');
+        throw createError(
+          404,
+          'The quote you are trying to update was not found.',
+        );
       }
 
       return updatedQuote;
@@ -81,14 +79,13 @@ class QuoteRepository {
 
   async deleteQuote(id: string): Promise<void> {
     try {
-      if (!mongoose.Types.ObjectId.isValid(id)) {
-        throw createError(400, 'Invalid ID format.');
-      }
-
       const result = await Quote.findByIdAndDelete(id);
 
       if (!result) {
-        throw createError(404, 'Quote not found.');
+        throw createError(
+          404,
+          'The quote you are trying to delete does not exist.',
+        );
       }
     } catch (error: any) {
       throw createError(500, `${error.message}`);
--- services/quotes.service.ts
+++ services/quotes.service.ts
@@ -4,20 +4,22 @@ import { IQuote } from '../models/Quote';
 import quoteRepository from '../repositories/quote.repository';
 
 class QuotesService {
-  async createQuote(quote: IQuote): Promise<IQuote> {
-    return quoteRepository.createQuote(quote);
+  async createQuote(quote: IQuote): Promise<Object> {
+    const createdQuote = await quoteRepository.createQuote(quote);
+
+    return {
+      id: createdQuote.id,
+      quote: createdQuote.quote,
+      author: createdQuote.author,
+      information: {
+        userID: createdQuote.userID,
+        status: createdQuote.status,
+      },
+    };
   }
 
-  async getQuotes(currentPage: number, perPage: number) {
-    return quoteRepository.getQuotes(currentPage, perPage);
-  }
-
-  async getRandomQuote(): Promise<IQuote | null> {
-    return await quoteRepository.getRandomQuote();
-  }
-
-  async getQuote(id: string) {
-    var getQuote = await quoteRepository.getQuote(id);
+  async getQuoteByID(id: string) {
+    const getQuote = await quoteRepository.getQuoteByID(id);
 
     if (!getQuote) {
       throw createError.NotFound('Quote not found.');
@@ -26,24 +28,90 @@ class QuotesService {
     return getQuote;
   }
 
-  async updateQuote(id: string, quote: IQuote) {
-    var getQuote = await this.getQuote(id);
+  async getQuotes(currentPage: number, perPage: number) {
+    const getQuotes = await quoteRepository.getQuotes(currentPage, perPage);
 
-    if (getQuote.userID.toString() !== quote.userID.toString()) {
-      throw createError.Unauthorized();
-    }
+    const quotes = getQuotes.quotes.map((quote) => ({
+      id: quote.id,
+      quote: quote.quote,
+      author: quote.author,
+      information: {
+        userID: quote.userID,
+      },
+    }));
 
-    return await quoteRepository.updateQuote(id, quote);
+    return {
+      quotes,
+      pagination: {
+        totalQuotes: getQuotes.totalQuotes,
+        totalPages: getQuotes.totalPages,
+        currentPage: getQuotes.currentPage,
+      },
+    };
   }
 
-  async deleteQuote(id: string, userID: string): Promise<void | null> {
-    var getQuote = await this.getQuote(id);
+  async getRandomQuote(): Promise<Object> {
+    const getRandomQuote = await quoteRepository.getRandomQuote();
 
-    if (getQuote?.userID.toString() !== userID) {
-      throw createError.Unauthorized();
+    if (!getRandomQuote) {
+      throw createError.NotFound('Quote not found.');
     }
 
-    await quoteRepository.deleteQuote(id);
+    return {
+      id: getRandomQuote.id,
+      quote: getRandomQuote.quote,
+      author: getRandomQuote.author,
+      information: {
+        userID: getRandomQuote.userID,
+      },
+    };
+  }
+
+  async getQuote(id: string) {
+    const getQuote = await this.getQuoteByID(id);
+
+    return {
+      id: getQuote.id,
+      quote: getQuote.quote,
+      author: getQuote.author,
+      information: {
+        userID: getQuote.userID,
+      },
+    };
+  }
+
+  async updateQuote(id: string, quote: IQuote) {
+    const getQuote = await this.getQuoteByID(id);
+
+    if (getQuote.userID.toString() !== quote.userID.toString()) {
+      throw createError.Unauthorized(
+        'You are not authorized to perform this action.',
+      );
+    }
+
+    const updatedQuote = await quoteRepository.updateQuote(getQuote.id, quote);
+
+    return {
+      id: updatedQuote.id,
+      quote: updatedQuote.quote,
+      author: updatedQuote.author,
+      information: {
+        userID: updatedQuote.userID,
+        status: updatedQuote.status,
+      },
+    };
+  }
+
+  async deleteQuote(id: string, userID: string): Promise<void> {
+    const getQuote = await this.getQuoteByID(id);
+
+    if (getQuote?.userID.toString() !== userID) {
+      throw createError.Unauthorized(
+        'You are not authorized to perform this action.',
+      );
+    }
+
+    await quoteRepository.deleteQuote(getQuote.id);
 
     return;
   }
