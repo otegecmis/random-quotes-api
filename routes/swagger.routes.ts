11c08b2f8e3a80a3be6d71630418c053a40f1076
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-11-19 19:25:29 +0300

    chore: add logger for `swagger availability` in `development` (#64)

--- routes/swagger.routes.ts
+++ routes/swagger.routes.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 
 import config from '../config/index.config';
+import logger from '../helpers/logger.helper';
 import { serve, setup } from '../config/swagger.config';
 
 const router = express.Router();
 
 if (config.server.node_env === 'development') {
   router.use('/', serve, setup);
+
+  logger.info('Swagger is available at /api/swagger');
 }
 
 export default router;
