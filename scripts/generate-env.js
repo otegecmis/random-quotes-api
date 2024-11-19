5a6d82b6db4aab2bec6ee5b22bc06c0962bf2d0f
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-11-19 18:05:56 +0300

    chore: improve output of `generate-env` script (#62)

--- scripts/generate-env.js
+++ scripts/generate-env.js
@@ -3,13 +3,15 @@ const path = require('path');
 
 const envContent = `PORT = 8000
 NODE_ENV = development
-MONGO_URI = mongodb://mongo:27017/quotes
 
-ACCESS_TOKEN_SECRET = access-token-secret
-REFRESH_TOKEN_SECRET = refresh-token-secret
+MONGO_URI = mongodb://mongo:27017/quotes
 
 SMTP_USER = smtp-user
 SMTP_PASS = smtp-pass
+
+ACCESS_TOKEN_SECRET = access-token-secret
+REFRESH_TOKEN_SECRET = refresh-token-secret
+PASSWORD_RESET_SECRET = password-reset-secret
 `;
 
 const filePath = path.resolve(__dirname, '../.env');
