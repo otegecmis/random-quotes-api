8a1c9ac40560179cfae4540961c468909647e617
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-11-21 16:16:04 +0300

    chore: update `generate env` script

--- scripts/env.js
+++ scripts/env.js
@@ -1,15 +1,31 @@
 const fs = require('fs');
 const path = require('path');
+const crypto = require('crypto');
 
 const envContent = `
+# Server configuration
 PORT = 8000
-NODE_ENV = development
+ORIGIN = * # Allowed origins for CORS (use '*' for all or specify origins.)
 
-MONGO_URI = mongodb://mongo:27017/quotes
+# Environment settings
+NODE_ENV = development # Set the environment: development, production, or test.
 
-ACCESS_TOKEN_SECRET = access-token-secret
-REFRESH_TOKEN_SECRET = refresh-token-secret
-PASSWORD_RESET_SECRET = password-reset-secret
+# Database configuration
+MONGO_URI = mongodb://mongo:27017/quotes # Use 'mongodb://localhost:27017/quotes' for local development.
+
+# SMTP credentials for email services
+SMTP_USER = smtp-user
+SMTP_PASS = smtp-pass
+
+# JWT token settings
+ACCESS_TOKEN_SECRET = ${crypto.randomBytes(32).toString('hex')}
+ACCESS_TOKEN_EXPIRATION = 1h
+
+REFRESH_TOKEN_SECRET = ${crypto.randomBytes(32).toString('hex')}
+REFRESH_TOKEN_EXPIRATION = 7d
+
+PASSWORD_RESET_SECRET = ${crypto.randomBytes(32).toString('hex')}
+PASSWORD_RESET_EXPIRATION = 15m
 `;
 
 const filePath = path.resolve(__dirname, '../.env');
@@ -20,5 +36,6 @@ fs.writeFile(filePath, envContent, 'utf8', (err) => {
     process.exit(1);
   }
 
-  console.log('🥳\n');
+  console.log('🥳 .env file created successfully.');
+  console.log('🚨 Update the .env file with your own values.');
 });
